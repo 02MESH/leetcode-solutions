095115318d9548065e0e0be15c0d761222fa0715
Meshwil Dias <meshwildias@gmail.com>
2023-10-13 13:48:48 +0100

    Solved 2 LL problems

---
+++ Easy/1688/1688.cpp
@@ -0,0 +1,14 @@
+int numberOfMatches(int n) {
+    int result = 0;
+    while(n > 1) {
+        if (n % 2 == 0) {
+            n = n / 2;
+            result += n;
+        }
+        else {
+            n = (n - 1) / 2;
+            result += n + 1;
+        }
+    }
+    return result;
+}
---
+++ Medium/2807/2807.cpp
@@ -0,0 +1,29 @@
+#include <algorithm> //has the gcd function here
+#include <iostream>
+
+ListNode* insertGreatestCommonDivisors(ListNode* head) {
+    auto node = head;
+    if (node->next)
+        node = node->next;
+    else 
+        return head;
+
+    auto travelNode = head;
+
+    while(node) {
+        //Calculate the gcd
+        int result = __gcd(travelNode->val, node->val);
+
+        //Create a new node and set the next pointer to the node
+        ListNode* newNode = new ListNode({result, node});
+            
+        //Establish the connection
+        travelNode->next = newNode;
+
+        //Move the pointers forward
+        travelNode = node;
+        node = node->next;
+    }
+
+    return head;
+}
