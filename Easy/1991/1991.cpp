b1280e137a23d6045532550434c1f646f5ff3197
Meshwil Dias <meshwildias@gmail.com>
2023-10-23 14:33:42 +0100

    Minor errors fixed

--- Easy/1991/1991.cpp
+++ Easy/1991/1991.cpp
@@ -1,14 +1,9 @@
 int findMiddleIndex(vector<int>& nums) {
-<<<<<<< HEAD
-    
-=======
->>>>>>> 31d8df287ea5fc40ad3169f620f39785462843bb
     int left = 0;
     int right = 0;
     for(auto x : nums) right+=x;
 
     for(int i = 0; nums.size() > i; i++) {
-<<<<<<< HEAD
         right = right - nums[i];
         if(left == right) {
             return i;
@@ -17,14 +12,3 @@ int findMiddleIndex(vector<int>& nums) {
     }
     return -1;
 }
-=======
-      right = right - nums[i];
-      if(left == right) {
-         return i;
-      }
-      left += nums[i];
-    }
-
-    return -1;
-}
->>>>>>> 31d8df287ea5fc40ad3169f620f39785462843bb
