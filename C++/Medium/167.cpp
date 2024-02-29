a539517183a21dbc3d068cd81ab528b60f73ab40
Meshwil Anthony Dias <meshwildias@gmail.com>
2024-02-29 00:11:59 +0000

    167.cpp modifications

--- C++/Medium/167.cpp
+++ C++/Medium/167.cpp
@@ -11,3 +11,31 @@ vector<int> twoSum(vector<int>& numbers, int target) {
     result[1] = j+1;
     return result;
 }
+
+//more space-efficient
+vector<int> twoSum(vector<int>& numbers, int target) {
+    unordered_map<int, int> hashmap;
+    int difference = 0;
+    for(int i = 0; numbers.size() > i; i++) {
+        difference = target - numbers[i];
+        if(auto search = hashmap.find(difference); search != hashmap.end())
+            return {(search->second+1), (i+1)};
+        else
+            hashmap[numbers[i]] = i;
+    }
+    return {-1,-1};
+}
+
+//hashmap approach
+vector<int> twoSum(vector<int>& numbers, int target) {
+    unordered_map<int, int> hashmap;
+    int difference = 0;
+    for(int i = 0; numbers.size() > i; i++) {
+        difference = target - numbers[i];
+        if(auto search = hashmap.find(difference); search != hashmap.end())
+            return {(search->second+1), (i+1)};
+        else
+            hashmap[numbers[i]] = i;
+    }
+    return {-1,-1};
+}
