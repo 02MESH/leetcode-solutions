61cff0919749ecbc1700531d043aba2c93771b7b
Meshwil Anthony Dias <02mesh@duck.com>
2024-08-14 13:29:32 +0100

    Solved l66

--- C++/Easy/67.cpp
+++ C++/Easy/67.cpp
@@ -1,23 +1,32 @@
-#include<string>
+#include <cctype> //https://en.cppreference.com/w/cpp/header/cctype
+#include <cstdio> //https://github.com/mfussenegger/nvim-dap/issues/1061#issuecomment-1773218073
+#include <cstring>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
 string addBinary(string a, string b) {
-    int i = a.size() - 1, j = b.size() - 1;
-    string result;
-    int carry = 0;
-    while(i >= 0 || j >= 0 || carry) {
-        int sum = carry;
-        if(i >= 0) {
-            sum += a[i] - '0';
-            i--;
-        }
-        if(j >= 0) {
-            sum += b[j] - '0';
-            j--;
-        }
-        carry = sum / 2;
-        result = to_string(sum % 2) + result;
-    }
-    return result;
+  setvbuf(stdout, nullptr, _IONBF, 0); // For debugging the buffer correctly)
+  int aSize = a.size() - 1, bSize = b.size() - 1;
+  string result;
+  int carry = 0;
+  while (aSize >= 0 || bSize >= 0 || carry > 0) {
+    int sum = carry;
+    if (aSize >= 0)
+      sum += a[aSize--] - '0';
+    if (bSize >= 0)
+      sum += b[bSize--] - '0';
+    carry = sum / 2;
+    result = to_string(sum % 2) + result;
+  }
+  return result;
+}
+
+int main() {
+  setvbuf(stdout, nullptr, _IONBF, 0); // For debugging the buffer correctly)
+  string test1 = "1010"; 
+  string test2 = "1011";
+  std::cout << addBinary(test1, test2) << std::endl;
+  return 1;
 }
